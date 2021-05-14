1c86dbfd6dd28f5f63dc9620cac1b379573b0fa8
ItzSD06 <82575492+ItzSD06@users.noreply.github.com>
2021-05-14 12:23:53 +0530

    Update random_article.py

--- random_article.py
+++ random_article.py
@@ -1,4 +1,4 @@
-import wikipedia
+import wikipedia #importing the module
 
 print("--Random Wikipedia Article Generator--")
 print("0. Exit")
@@ -6,15 +6,15 @@ print("1. Search New Topic")
 
 while True:
     print("\n")
-    choice = int(input("Enter 0 for Exit and 1 for New Topic (0 or 1): "))
+    choice = int(input("Enter 0 for Exit and 1 for New Topic (0 or 1): "))#new topic or exit
     print("\n")
-    if choice == 0:
+    if choice == 0:#if-else statement
         print("We can't wait to see you again, bye bye...")
         break
     elif choice == 1:
         title = wikipedia.random(1)
         print("Would you like to read about - ", title)
-        while True:
+        while True:#while True loop
             confirmchoice = input("Enter y for yes and n for no (y or n): ")
             if confirmchoice == "y" or confirmchoice == "Y":
                 summary = wikipedia.summary(title)
